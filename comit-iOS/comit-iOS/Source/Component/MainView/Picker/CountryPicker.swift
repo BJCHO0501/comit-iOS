2364057b03417bf83c4c0da072fc9afad33db84b
BJCHO0501 <bjcho1503@naver.com>
2022-10-20 21:34:37 +0900

    📝 :: CountryPicker

---
+++ comit-iOS/comit-iOS/Source/Component/MainView/Picker/CountryPicker.swift
@@ -0,0 +1,42 @@
+//
+//  CountryPicker.swift
+//  comit-iOS
+//
+//  Created by 조병진 on 2022/10/20.
+//
+
+import SwiftUI
+
+struct CountryPicker: View {
+    @Binding var selectCountry: Country
+    var ignoreCountry: Country
+    private var countryArr: [Country] = [.KR, .JP, .USA, .PL]
+    
+    init(
+        selectCountry: Binding<Country>,
+        ignoreCountry: Country
+    ) {
+        self._selectCountry = selectCountry
+        self.ignoreCountry = ignoreCountry
+    }
+    
+    var body: some View {
+        Picker("선택하세요", selection: $selectCountry) {
+            ForEach(countryArr, id: \.self) {
+                if $0 != ignoreCountry {
+                    Text($0.KRName() + "(\($0.unit()))")
+                }
+            }
+        }
+        .pickerStyle(MenuPickerStyle())
+    }
+}
+
+struct CountryPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryPicker(
+            selectCountry: .constant(Country.USA),
+            ignoreCountry: Country.JP
+        )
+    }
+}
