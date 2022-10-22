54a5f636c8354a2a9316f597cd8bceefd7831d95
BJCHO0501 <bjcho1503@naver.com>
2022-10-22 21:13:20 +0900

    📝 :: comit_iOSApp

--- comit-iOS/comit-iOS/Application/comit_iOSApp.swift
+++ comit-iOS/comit-iOS/Application/comit_iOSApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct comit_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            NavigationView {
+                MainView()
+            }
         }
     }
 }
